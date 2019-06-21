f6dc7d1504d4509ffe91c57442bb75af67b309bf
agent1red <darklen14@gmail.com>
2019-06-20 22:37:31 -0700

    Added entry, entry-list components with styles to add picture. Added a barrel file for exporting from entries Dir

---
+++ src/app/entries/entry/entry.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-entry',
+    templateUrl: 'entry.component.html',
+    styleUrls: ['entry.component.css']
+})
+export class EntryComponent {
+    title: string = 'My First Photo';
+    photo: string = 'http://placehold.it/800x500?text= Angular Basics';
+    description: string = 'A description of my First photo';
+}
