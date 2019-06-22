00f00711c21bfd63ad4e7e99e5a3bbc0003a3905
agent1red <darklen14@gmail.com>
2019-06-22 16:36:05 -0700

    update files

--- src/app/entries/entry/entry-comment-form-component.ts
+++ src/app/entries/entry/entry-comment-form-component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { NgForm } from "@angular/forms";
-
+import { EntryService } from '../shared/entry.service';
 
 
 
@@ -12,12 +12,19 @@ import { NgForm } from "@angular/forms";
 export class EntryCommentFormComponent {
     name: string = "";
     comment: string = "";
+    @Input() entryId: number;
     @Output() onCommentAdded = new EventEmitter<{ name: string; comment: string; }>();
     @ViewChild('commentForm')commentForm: NgForm;
 
+    constructor(private entryService: EntryService) {
+
+    }
     onSubmit(commentForm: NgForm) {
         let comment = { name: this.name, comment: this.comment };
-        this.onCommentAdded.emit(comment);
-        this.commentForm.resetForm();
+        this.entryService.addComment(this.entryId, comment).then(() => {
+            this.onCommentAdded.emit(comment);
+            this.commentForm.resetForm();
+        });
+        
     }
 }
