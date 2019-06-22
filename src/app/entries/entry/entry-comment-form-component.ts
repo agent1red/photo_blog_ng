a80c54b3bde9366b4cceb2effda1925c2f07efc6
agent1red <darklen14@gmail.com>
2019-06-22 16:18:10 -0700

    Added form reset after submitting data in comments component section

--- src/app/entries/entry/entry-comment-form-component.ts
+++ src/app/entries/entry/entry-comment-form-component.ts
@@ -1,4 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { NgForm } from "@angular/forms";
+
+
 
 
 @Component({
@@ -10,10 +13,11 @@ export class EntryCommentFormComponent {
     name: string = "";
     comment: string = "";
     @Output() onCommentAdded = new EventEmitter<{ name: string; comment: string; }>();
+    @ViewChild('commentForm')commentForm: NgForm;
 
-
-    onSubmit() {
+    onSubmit(commentForm: NgForm) {
         let comment = { name: this.name, comment: this.comment };
         this.onCommentAdded.emit(comment);
+        this.commentForm.resetForm();
     }
 }
